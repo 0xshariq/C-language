b6f89a36b041f6894bbcca10ff8e2b9a1db9d936
0xshariq <145042629+0xshariq@users.noreply.github.com>
2023-10-25 14:47:31 +0000

    Initial commit

---
+++ operators.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+void main(){
+  int a, b, c, d, e, f;
+  clrscr();
+  c = a & b;
+  d = a << b;
+  e = a >> b;
+  f = a ^ b;
+  printf("%d",c);
+  printf("%d",d);
+  printf("%d",e);
+  printf("%d",f);
+  return 0;
+  getch() ;
+}

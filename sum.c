bd7871fcc46f0b7e91f447b8a196653562210816
0xshariq <145042629+0xshariq@users.noreply.github.com>
2023-11-16 16:45:09 +0000

    New file

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#inculde <conio.h>
+void main(){
+  int a,b,sum;
+  printf("Enter the value of a");
+  printf("Enter the value of b");
+  scanf("%d %d",&a,&b);
+  sum = a + b;
+  printf("Sum of a and b=%d",sum);
+  getch();
+}

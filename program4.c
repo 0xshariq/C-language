33a963536f531127bcfb613b62f3365ba12b1362
0xshariq <145042629+0xshariq@users.noreply.github.com>
2023-12-29 05:30:40 +0000

    New

--- program4.c
+++ program4.c
@@ -1,17 +1,18 @@
 #include<stdio.h>
-void main()
+int main()
 {
   int a[20],i;
   int even=0,odd=0;
   printf("Enter elements of the array:");
   for(i=0;i<10;i++)
   {
-    scanf("%d",a[i]);
+    scanf("%d",&a[i]);
     if(a[i]%2==0)
       even++;
     else
       odd++;
   }
   printf("Total even numbers are: %d",even);
+  printf("\n");
   printf("Total odd numbers are: %d", odd);
 }

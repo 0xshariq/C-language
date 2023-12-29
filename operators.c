4fc3b7ac251b4a5ac84a32442b8d3c2f6c2062b1
0xshariq <145042629+0xshariq@users.noreply.github.com>
2023-12-29 05:11:39 +0000

    New

--- operators.c
+++ operators.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
-int main(){
+
+int main() {
   int a, b, c, d, e, f;
-  scanf("Enter the value of a : %d",&a);
-  scanf("Enter the value of b : %d",&b);
+
+  printf("Enter the value of a : ");
+  scanf("%d", &a);
+
+  printf("Enter the value of b : ");
+  scanf("%d", &b);
+
   c = a & b;
   d = a << b;
   e = a >> b;
   f = a ^ b;
-  printf("%d",c);
-  printf("%d",d);
-  printf("%d",e);
-  printf("%d",f);
+
+  printf("%d\n", c);
+  printf("%d\n", d);
+  printf("%d\n", e);
+  printf("%d\n", f);
+
   return 0;
 }
